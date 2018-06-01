b4f4b1a779c1fba69994034bc07e2f4f8da0c8d4
kyome <aaaicu@naver.com>
2018-06-01 10:39:19 +0900

    파일 경로 변경( 절대경로 -> 상대경로)

--- Moocl/src/main/java/kr/co/moocl/dao/MovieDao.java
+++ Moocl/src/main/java/kr/co/moocl/dao/MovieDao.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -19,9 +20,6 @@ import org.springframework.data.mongodb.core.script.ExecutableMongoScript;
 import org.springframework.stereotype.Component;
 
 import kr.co.moocl.vo.InteMovieVo;
-import kr.co.moocl.vo.ReviewVo;
-
-
 
 @Component
 public class MovieDao {
@@ -30,128 +28,137 @@ public class MovieDao {
 	MongoTemplate mongoTemplate;
 	@Autowired
 	MongoOperations mongoOperation;
-	
-		
+	@Autowired
+	private ResourceLoader resourceLoader;
+
 	public List<InteMovieVo> getMovieInfoList(String keyword) {
-			
+
 		Query query = new Query().with(new Sort(Sort.Direction.DESC, "open_date"));
-		
-		query.limit(10);  //10개씩 불러오고 스크롤 내리면 더불러오게 나중에 코딩
-		
+
+		query.limit(10); // 10개씩 불러오고 스크롤 내리면 더불러오게 나중에 코딩
+
 		query.addCriteria(Criteria.where("inte_title").regex(keyword));
-		
-		List<InteMovieVo> queryResult = mongoTemplate.find(query, InteMovieVo.class, "movie_info"); 
-		
+
+		List<InteMovieVo> queryResult = mongoTemplate.find(query, InteMovieVo.class, "movie_info");
+
 		return queryResult;
 	}
 
 	public InteMovieVo getMovieInfoById(String movieId) {
-		
+
 		Criteria criteria = new Criteria("_id");
 		criteria.is(movieId);
-		
+
 		Query query = new Query(criteria);
-		
+
 		InteMovieVo rawMovieData = mongoTemplate.findOne(query, InteMovieVo.class, "movie_info");
-	
+
 		return rawMovieData;
 	}
-	
-	public List<InteMovieVo> getRelatedMoviesByPersonId(int personId){
-		
+
+	public List<InteMovieVo> getRelatedMoviesByPersonId(int personId) {
+
 		Criteria criteria = new Criteria("person");
 		Criteria embededCriteria = new Criteria("person_id");
 		embededCriteria.is(personId);
 		criteria.elemMatch(embededCriteria);
-		
+
 		Query query = new Query().with(new Sort(Sort.Direction.DESC, "open_date"));
 		query.addCriteria(criteria);
-		
+
 		List<InteMovieVo> relatedMovieList = mongoTemplate.find(query, InteMovieVo.class, "movie_info");
 		return relatedMovieList;
 	}
-	
-	public List<Document> getRelatedMoviesByMovieId(String stringMovieId) {
-	    StringBuilder text = new StringBuilder();
-	    BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader(
-			        new File("C:\\Users\\1\\Desktop\\Bit_Project\\MooCl\\Moocl\\frontend\\src\\store\\filmo_script.js")));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    try {
-	        while (true) {
-	            String line= "";
-				try {
-					line = br.readLine();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-	            if (line == null)
-	                break;
-	            text.append(line).append("\n");
-	        }
-	    } finally {
-	        try { br.close(); } catch (Exception ignore) {}
-	    }
-	    ExecutableMongoScript filmoScript = new ExecutableMongoScript(text.toString());
-	    List<Document> movieList= (List<Document>) mongoOperation.scriptOps().execute(filmoScript, stringMovieId); 
-        return movieList;
-	}
+// 사용하지 않음
+//	public List<Document> getRelatedMoviesByMovieId(String stringMovieId) {
+//		StringBuilder text = new StringBuilder();
+//		BufferedReader br = null;
+//		try {
+//			br = new BufferedReader(new FileReader(new File(
+//					resourceLoader.getResource("classpath:JavaScript/filmo_script.js").getURI().getPath())));
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		try {
+//			while (true) {
+//				String line = "";
+//				try {
+//					line = br.readLine();
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				if (line == null)
+//					break;
+//				text.append(line).append("\n");
+//			}
+//		} finally {
+//			try {
+//				br.close();
+//			} catch (Exception ignore) {
+//			}
+//		}
+//		ExecutableMongoScript filmoScript = new ExecutableMongoScript(text.toString());
+//		List<Document> movieList = (List<Document>) mongoOperation.scriptOps().execute(filmoScript, stringMovieId);
+//		return movieList;
+//	}
 
 	public List<InteMovieVo> getMovieInfoList(List<String> movieIds) {
 		Criteria criteria = new Criteria("_id");
 		criteria.in(movieIds);
-		
+
 		Query query = new Query(criteria);
-		
+
 		List<InteMovieVo> movieList = mongoTemplate.find(query, InteMovieVo.class, "movie_info");
 		return movieList;
 	}
 
 	public List<Document> getWordListByMovieId(String movieId) {
-	    StringBuilder text = new StringBuilder();
-	    BufferedReader br = null;
+		StringBuilder text = new StringBuilder();
+		BufferedReader br = null;
 		try {
-			br = new BufferedReader(new FileReader(
-			        new File("C:\\Users\\1\\git\\MooCl\\Moocl\\src\\main\\resources\\JavaScript\\getWordListByMovieId.js")));
+			br = new BufferedReader(new FileReader(new File(
+					resourceLoader.getResource("classpath:JavaScript/getWordListByMovieId.js").getURI().getPath())));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-	    try {
-	        while (true) {
-	            String line= "";
+		try {
+			while (true) {
+				String line = "";
 				try {
 					line = br.readLine();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-	            if (line == null)
-	                break;
-	            text.append(line).append("\n");
-	        }
-	    } finally {
-	        try { br.close(); } catch (Exception ignore) {}
-	    }
-	    ExecutableMongoScript wordcloudScript = new ExecutableMongoScript(text.toString());
-	    List<Document> movieList= (List<Document>) mongoOperation.scriptOps().execute(wordcloudScript, movieId); 
-        return movieList;
+				if (line == null)
+					break;
+				text.append(line).append("\n");
+			}
+		} finally {
+			try {
+				br.close();
+			} catch (Exception ignore) {
+			}
+		}
+		ExecutableMongoScript wordcloudScript = new ExecutableMongoScript(text.toString());
+		List<Document> movieList = (List<Document>) mongoOperation.scriptOps().execute(wordcloudScript, movieId);
+		return movieList;
 	}
-	
+
 	public void updateCloudWord(String movieId, Object result, String date) {
 
-		Query query = new Query( new Criteria("_id").is(movieId));
+		Query query = new Query(new Criteria("_id").is(movieId));
 		Update update = new Update();
 		update.set("wordcloudList", result);
 		update.set("wordcloudDate", date);
-		
-		mongoTemplate.updateFirst(query, update,"movie_info");
+
+		mongoTemplate.updateFirst(query, update, "movie_info");
 		System.out.println(mongoTemplate.find(query, InteMovieVo.class, "movie_info"));
 	}
-	
+
 }
